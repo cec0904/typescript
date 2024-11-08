0b78908e2cc247513904f01a1f8881ecc96d8bdc
cec0904 <cec0904@naver.com>
2024-11-08 16:37:53 +0900

    11.08

--- src/components/October thirty-first.jsx
+++ src/components/October thirty-first.jsx
@@ -1,5 +1,6 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs, orderBy } from "firebase/firestore";
 import { firestore } from "../firebaseConfig";
+import { useEffect } from "react";
 
 
 const TextArea = styled.div`
@@ -36,6 +37,60 @@ const SubmitButton = styled.input`
     }
 
 `;
+
+
+
+const fetchPosts = async() => {
+    //1. Firebase에 필요한 게시글 받아오기 쿼리(Query)
+    const path = collection(firestore,"posts");
+    const condition = orderBy("createdAt", 'desc');
+    const postsQuery = query(path,condtion);
+    //2. 쿼리에 맞는 Doc'들' 가져오기
+    const snapshot = await getDocs(postsQuery);
+    //3. 가져온 Doc들 Timeline에 쓸 수 있도록 가공(=형태)
+    const timelinePosts = snapshot.docs.map((doc)=>{
+        //3-1. doc 안에서 필요한 데이터를 뽑아온다.
+        const {post, userId, nickname, createdAt }
+            = doc.data() as IPost;
+        //3-2. 뽑아온 데이터를 반환한다.(객체형태로)
+        return{
+            post : post,
+            userId : userId,
+            nickname : nickname,
+            createdAt : createdAt,
+            id : doc.id
+        }
+        // key 와 value 같으면 생략해도 이해함
+        //ex) post,
+        //    userId,
+        //    nickname ...
+    });
+    //4. 가공된 데이터를 State 저장
+    setPosts(timelinePosts);
+}
+
+
+//Page Design Rendering
+
+return (
+    <Container>
+        {posts.map((post) => {
+            return(
+                <div>
+                    <h1>{post.post}</h1>
+                </div>
+            );
+        })}
+    </Container>
+);
+
+//접속할 때 마다, Timeline이 보여질 때마다
+useEffect(() => {
+
+})
+
+
+
 
 export default () => {
     // Page Logic Process
